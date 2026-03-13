234b1c62859cf556035cac9c1e74fd85e8c13bc7
Eduardo Guzman Maldonado <eduardo.guzman@innovamics.com>
2026-03-13 10:12:30 +0100

    feat: avancement exo8 (affichage)

--- main_exo8.c
+++ main_exo8.c
@@ -23,9 +23,14 @@ void ajouter(Coord* tab[6], int tPhys, int* tLog) {
     tab[*tLog] = temp;
     (*tLog)++;
 }
-void afficher(Coord** tab, int tLog) {
 
+void afficher(Coord* tab[6], int tLog) {
+    printf("--- Liste de coordonées :\n");
 
+    for (int i = 0; i < tLog; ++i) {
+        printf("Point %d (%d,%d)\n", i, tab[i]->x, tab[i]->y);
+    }
+    printf("--- Fin de la liste\n");
 }
 
 int main() {
@@ -49,12 +54,16 @@ int main() {
                 ajouter(tab, 6, &nbCoord);
                 break;
             case 2:
-                printf("Choix 2");
+                afficher(tab, nbCoord);
                 break;
             default:
                 break;
         } // fin du switch
     } while (choix < 3 && choix > 0);
+
+
+    // Liberation
+
 
     return 0;
 }
